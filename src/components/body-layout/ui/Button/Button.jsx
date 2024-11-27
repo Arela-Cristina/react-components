a5a006a6569fcdc811ddac628529cfc65b79605d
Arela-Cristina <instagrandine@gmail.com>
2024-11-27 22:22:04 +0100

    create and add style

--- src/components/body-layout/ui/Button/Button.jsx
+++ src/components/body-layout/ui/Button/Button.jsx
@@ -1,6 +1,7 @@
+import style from "./Button.module.css";
+
 export default function Button() {
-    return (
-      <button>Click</button>
-    );
-  }
-  
+  return (
+    <button className={ style.kawaiButton }>Click</button>
+  );
+}
