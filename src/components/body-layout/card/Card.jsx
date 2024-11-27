1b090bd0dd8a847fc718a70c4f5da3b684d21807
Arela-Cristina <instagrandine@gmail.com>
2024-11-27 22:02:41 +0100

    import Button to Card,jsx

--- src/components/body-layout/card/Card.jsx
+++ src/components/body-layout/card/Card.jsx
@@ -1,5 +1,6 @@
 import style from "./Card.module.css";
 import Mandy from "../../../assets/Brawl_Mandy.png";
+import Button from "../ui/Button/Button.jsx";
 
 export default function Card() {
   return (
@@ -8,6 +9,7 @@ export default function Card() {
       <div class={style.cardBody}>
         <h3 class="card-title">Card Title</h3>
         <p class="card-text">This is a short description</p>
+        <Button />
       </div>
     </div>
   );
