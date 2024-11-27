ad1ce40ff386993f0d7679ceea8bcbc6e7df8aa4
Arela-Cristina <instagrandine@gmail.com>
2024-11-27 17:58:32 +0100

    fix error

--- src/components/header-layout/Header.jsx
+++ src/components/header-layout/Header.jsx
@@ -3,7 +3,9 @@ import style from "./header.module.css";
 export default function Header() {
   return (
     <header>
-      <div className= { style.header } >My blog</div>
+      <div className={style.backgroundColor}>
+        <div className={style.header}>My blog</div>
+      </div>
     </header>
   );
 }
