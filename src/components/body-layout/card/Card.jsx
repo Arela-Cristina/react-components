c1738ccdc630f918f027fe56118cab1f4baa2c50
Arela-Cristina <instagrandine@gmail.com>
2024-11-27 21:44:18 +0100

    add style for cardBody

--- src/components/body-layout/card/Card.jsx
+++ src/components/body-layout/card/Card.jsx
@@ -5,7 +5,7 @@ export default function Card() {
   return (
     <div class={style.card}>
       <img className={style.thumb} src={Mandy} alt="Mandys candies" />
-      <div class="card-body">
+      <div class={style.cardBody}>
         <h3 class="card-title">Card Title</h3>
         <p class="card-text">This is a short description</p>
       </div>
