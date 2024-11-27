484ce967ec637a7945333b7f703251f4b774a40f
Arela-Cristina <instagrandine@gmail.com>
2024-11-27 15:59:04 +0100

    Add basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import "./App.css"; 
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <h1>Hola Mundo!</h1>
+      <h1>titolo</h1>
+
+      <h1>body</h1>
+      <div>card</div>
+
+      <h1>footer</h1>
     </div>
   );
 }
